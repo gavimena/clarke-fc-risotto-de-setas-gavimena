78e19374adff3386c7853916a2fc839a028a2fb6
Gabi Mena <gabriela.grojas@gmail.com>
2018-04-08 16:29:35 +0200

    replace display flex for grid to the item container

--- js/index.js
+++ js/index.js
@@ -4,9 +4,12 @@ let mainTitle = document.getElementById('main-title');
 let itemListContainer =
 document.querySelector('.itemsList__container');
 let check = document.querySelector('.item__checkbox');
-console.log(itemListContainer);
 let input = document.querySelector('.item__input');
+let selectAll = document.getElementById('select-all');
+let deselectAll = document.getElementById('deselect-all');
 
+selectAll.addEventListener('click', toggleSelection(true));
+deselectAll.addEventListener('click', toggleSelection(false));
 
 fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
 	.then(function(response){
@@ -23,7 +26,6 @@ function renderItems(itemsJson) {
 	mainTitle.innerHTML = recipe.name;
 
 	for (let i=0; i < ingredients.length; i++) {
-		//itemListContainer.innerHTML += renderIngredient(ingredients[i]);
 		let itemElement = document.createElement('div');
 		itemElement.innerHTML = renderIngredient(ingredients[i]);
 		itemListContainer.append(itemElement);
@@ -40,10 +42,10 @@ function registerEvents(){
 	});
 }
 
-function selectAll(){
+function toggleSelection(value){
 	let checkBoxList = document.querySelectorAll('.item__checkbox');
 	checkBoxList.forEach(function(checkbox){
-		checkbox.checked = true;
+		checkbox.checked = value;
 	});
 }
 
@@ -56,7 +58,7 @@ function renderIngredient(ingredient){
 	<div class="item__container">
   		<input class="item__checkbox" type="checkbox" name="add-ingredient">
   		<input class="item__input" type="text" name="number-ingredients" value="${ingredient.items}">
-  		<div class="item__details">
+  		<div>
   			<h4 class="roboto-slab">${ingredient.product}</h4>
   			<p class="item__paragraph-c7">${ingredient.brand}</p>
   			<p class="item__paragraph-grey">${ingredient.quantity}</p>
@@ -65,6 +67,6 @@ function renderIngredient(ingredient){
   	</div>`;
 }
 
-return {};
+// return {};
 
 }();
