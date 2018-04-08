2368dbd0268faa3f1947f5b04807b4507f1c05b2
Gabi Mena <gabriela.grojas@gmail.com>
2018-04-08 15:53:13 +0200

    add function to render html and function to handle checkboxes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//get all html elements needed
+var app = function (config) {
 
 let mainTitle = document.getElementById('main-title');
 let itemListContainer =
@@ -13,16 +13,58 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
 		return response.json();
 	})
 	.then(function(json){
-		const recipe = json.recipe;
-		let ingredients = json.recipe.ingredients;
-		mainTitle.innerHTML = recipe.name;
+		renderItems(json);
+	});
 
-		for (let i=0; i<=ingredients.length; i++) {
-		itemListContainer.innerHTML += '<div class="item__container"><input class="item__checkbox" type="checkbox" value="" name="add-ingredient" onchange=checked()><input class="item__input" type="text" name="number-ingredients" value=""><div class="item__details"><h4 class="roboto-slab">' +  ingredients[i].product + '</h4><p class="item__paragraph-c7">' + ingredients[i].brand + '</p><p class="item__paragraph-grey">' + ingredients[i].quantity + '</p></div><h3 class="item__price">' + ingredients[i].price + '&nbsp€ </h3></div></div>'
-		}
-});
+function renderItems(itemsJson) {
+	console.log(itemsJson);
+	const recipe = itemsJson.recipe;
+	const ingredients = recipe.ingredients;
+	mainTitle.innerHTML = recipe.name;
 
-
-	function checked() {
-		alert('has cambiado');
+	for (let i=0; i < ingredients.length; i++) {
+		//itemListContainer.innerHTML += renderIngredient(ingredients[i]);
+		let itemElement = document.createElement('div');
+		itemElement.innerHTML = renderIngredient(ingredients[i]);
+		itemListContainer.append(itemElement);
 	}
+
+	registerEvents();
+}
+
+function registerEvents(){
+	let checkBoxList = document.querySelectorAll('.item__checkbox');
+	checkBoxList.forEach(function(checkbox){
+		console.log(checkbox);
+		checkbox.addEventListener('click', handleCheckIngredient)
+	});
+}
+
+function selectAll(){
+	let checkBoxList = document.querySelectorAll('.item__checkbox');
+	checkBoxList.forEach(function(checkbox){
+		checkbox.checked = true;
+	});
+}
+
+function handleCheckIngredient(e){
+	console.log('handleCheckIngredient', e);
+}
+
+function renderIngredient(ingredient){
+	return `
+	<div class="item__container">
+  		<input class="item__checkbox" type="checkbox" name="add-ingredient">
+  		<input class="item__input" type="text" name="number-ingredients" value="${ingredient.items}">
+  		<div class="item__details">
+  			<h4 class="roboto-slab">${ingredient.product}</h4>
+  			<p class="item__paragraph-c7">${ingredient.brand}</p>
+  			<p class="item__paragraph-grey">${ingredient.quantity}</p>
+  		</div>
+  		<h3 class="item__price">${ingredient.price} €</h3>
+  	</div>`;
+}
+
+return {};
+
+}();
